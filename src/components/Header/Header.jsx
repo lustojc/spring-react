4174c896f47a707c089a6f3de401423943c62326
lustojc <sashalusto228@gmail.com>
2022-03-13 14:02:11 +0300

    Added burger-menu logic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { MenuItems } from "./MenuItems";
 import logo from "../../assets/img/spring-logo.svg";
 import "./header.css";
 import Dropdown from "./Dropdown";
+import MobileMenu from "../MobileMenu/MobileMenu";
+
+
 const Header = () => {
-    
+  
+  const [isMobile, setIsMobile] = useState(false)
+
 
   return (
     <header>
       <nav className="nav">
         <div className="container nav-wrapper dp-flex jc-betw">
-          <a href="">
+          <a className="mg-bt_20" href="">
             <img className="logo" src={logo} alt="Spring-logo" />
           </a>
-          <div id="navbar" className="navbar hide">
+          <div id="navbar" onClick={()=> setIsMobile(false)} className={isMobile ? "navbar-mobile" : "navbar"}>
             <div className="dp-flex jc-betw" id="dropdown">
-              {MenuItems.map((el, index) => {
+              {isMobile 
+              ? <MobileMenu/>
+              : MenuItems.map((el, index) => {
                 return <Dropdown element={el} content={el.options} id={el.id} key={index} />;
-              })}
+              })
+              }
             </div>
           </div>
         </div>
       </nav>
+      <div onClick={()=> setIsMobile(!isMobile)} className={isMobile ? "burger-menu open" : "burger-menu"}>
+          {isMobile ? <div  className="burger-line"></div> : <div className="burger-line dp-hide"></div>}
+      </div>
     </header>
   );
 };
